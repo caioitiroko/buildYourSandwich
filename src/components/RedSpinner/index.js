5e3422e92bea7227c5bb2cb7af5e383c351911c9
Caio Yoshio Itiroko <caio.itiroko@tagview.com.br>
2019-05-01 15:36:05 -0300

    Update Loading component

--- src/components/RedSpinner/index.js
+++ src/components/RedSpinner/index.js
@@ -1,14 +1,21 @@
 import {
   ActivityIndicator,
+  ImageBackground,
   SafeAreaView,
+  View,
 } from "react-native";
 import React from "react";
 import styles from "./style";
+import WoodBackground from "../../../images/wood.jpg";
 
 const RedSpinner = () => (
-  <SafeAreaView style={styles.container}>
-    <ActivityIndicator size="large" color="#F00" />
-  </SafeAreaView>
+  <ImageBackground source={WoodBackground} style={styles.background}>
+    <SafeAreaView />
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#F00" />
+    </View>
+    <SafeAreaView />
+  </ImageBackground>
 );
 
 export default RedSpinner;
--- src/components/RedSpinner/style.js
+++ src/components/RedSpinner/style.js
@@ -1,6 +1,11 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  background: {
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+  },
   container: {
     alignItems: "center",
     flex: 1,
